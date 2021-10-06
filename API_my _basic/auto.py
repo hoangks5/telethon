dc2cf4f9123fd81e8d9675794a8af0c71bc7e7b6
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2021-10-06 20:19:30 +0700

    Add files via upload

---
+++ API_my _basic/auto.py
@@ -0,0 +1,51 @@
+from asyncio import events
+from os import name
+import re
+from telethon import TelegramClient, events, hints
+from telethon.tl.functions.channels import JoinChannelRequest
+import time
+from telethon.tl.functions.messages import SendMessageRequest
+import random
+from telethon.tl.types import messages
+from telethon.tl.functions.account import UpdateUsernameRequest
+from telethon.tl.functions.photos import UploadProfilePhotoRequest
+from telethon.tl.functions.account import UpdateProfileRequest
+import os
+
+
+f = open('chat.txt','r',encoding='utf-8')
+f = f.read()
+f = f.splitlines()
+acc = []
+os.chdir('./')
+list = os.listdir()
+for accs in list:
+    if accs[:3] == 'acc':
+        acc.append(accs.split('.')[0])
+def xinchao():
+    xinch = open('hello.txt','r',encoding='utf-8')
+    xinch = xinch.read()
+    xinch = xinch.splitlines()
+    for iz in xinch:
+        print(iz)
+xinchao()
+print('Đã đăng nhập '+str(len(acc))+' acc trên hệ thống, vui lòng đọc hướng dẫn chi tiết bên trên.\n')
+link_gr = input('Nhập link group(vd: https://t.me/laucua) :')
+time_acc = int(input('Thời gian chat cách nhau của các acc(giây) :'))
+api_id = 8741581
+api_hash = 'e86158b878659756b24fad85fe6fb18c'
+
+while True:
+    for accs in acc:
+        try:
+            client = TelegramClient(accs, api_id, api_hash)
+            async def main():
+                # Getting information about yourself
+                me = await client.get_me()
+                await client(JoinChannelRequest(link_gr))
+                refust = await client(SendMessageRequest(link_gr,random.choice(f)))
+            with client:
+                client.loop.run_until_complete(main())
+            time.sleep(time_acc)
+        except:
+            pass
