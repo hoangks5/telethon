fdea438415dc49d48eafaa6b2cec1c49e84ecde7
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2021-10-10 21:59:05 +0700

    Add files via upload

--- API_my _basic/auto.py
+++ API_my _basic/auto.py
@@ -11,11 +11,11 @@ from telethon.tl.functions.account import UpdateUsernameRequest
 from telethon.tl.functions.photos import UploadProfilePhotoRequest
 from telethon.tl.functions.account import UpdateProfileRequest
 import os
-
+import threading
 
 f = open('chat.txt','r',encoding='utf-8')
 f = f.read()
-f = f.splitlines()
+f = f.split('||')
 acc = []
 os.chdir('./')
 list = os.listdir()
@@ -23,7 +23,7 @@ for accs in list:
     if accs[:3] == 'acc':
         acc.append(accs.split('.')[0])
 def xinchao():
-    xinch = open('hello.txt','r',encoding='utf-8')
+    xinch = open('setting/hello.txt','r',encoding='utf-8')
     xinch = xinch.read()
     xinch = xinch.splitlines()
     for iz in xinch:
@@ -31,21 +31,41 @@ def xinchao():
 xinchao()
 print('Đã đăng nhập '+str(len(acc))+' acc trên hệ thống, vui lòng đọc hướng dẫn chi tiết bên trên.\n')
 link_gr = input('Nhập link group(vd: https://t.me/laucua) :')
-time_acc = int(input('Thời gian chat cách nhau của các acc(giây) :'))
-api_id = 8741581
-api_hash = 'e86158b878659756b24fad85fe6fb18c'
+try:
+    list_grs = link_gr.split(';')
+except:
+    list_grs = link_gr
 
-while True:
-    for accs in acc:
-        try:
-            client = TelegramClient(accs, api_id, api_hash)
-            async def main():
-                # Getting information about yourself
-                me = await client.get_me()
-                await client(JoinChannelRequest(link_gr))
-                refust = await client(SendMessageRequest(link_gr,random.choice(f)))
-            with client:
-                client.loop.run_until_complete(main())
-            time.sleep(time_acc)
-        except:
-            pass
+time_acc = int(input('Thời gian chat cách nhau của các acc(giây) :'))
+api_id = 8763319
+api_hash = '28f53a40a4052cd950dab693a3a3c04c'
+thres = []
+for accs in acc:
+    print('Đăng nhập '+accs)
+    client = TelegramClient(accs, api_id, api_hash)
+    async def main():
+                            # Getting information about yourself
+                        me = await client.get_me()
+    with client:
+                        client.loop.run_until_complete(main())
+print('Đăng nhập thành công '+str(len(acc))+' acc')
+def ok():
+    while True:
+        for az in list_grs:
+            for accs in acc:
+                try:
+                    client = TelegramClient(accs, api_id, api_hash)
+                    async def main():
+                            # Getting information about yourself
+                        me = await client.get_me()
+                        await client(JoinChannelRequest(az))
+                        await client(SendMessageRequest(az,random.choice(f)))
+                    with client:
+                        client.loop.run_until_complete(main())
+                    time.sleep(time_acc)
+                except:
+                    pass
+                
+ok()
+
+
